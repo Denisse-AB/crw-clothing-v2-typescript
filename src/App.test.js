ab016ed7732c6d546cfd6f6d7bf2573a0ef01a04
Denisse Abreu <denisseabreu21@yahoo.com>
2022-12-20 19:44:19 -0400

    Update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,58 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import Navigation from './routes/navigation/navigation';
+import Home from './routes/home.tsx';
+import SignInUp from './routes/sign-in-up/signInUp';
+import CheckoutPage from './routes/checkout/checkout.component';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<App />', () => {
+  it('Renders the navigation bar', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navigation />
+        </BrowserRouter>
+      </Provider>
+    );
+    const text = screen.getByText(/Sign In/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  it('Renders the Home page', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </Provider>
+    );
+    const text = screen.getAllByText(/Shop Now/i);
+    expect(text).toBeTruthy();
+  });
+
+  it('Renders the sign in and up page', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <SignInUp />
+        </BrowserRouter>
+      </Provider>
+    );
+    const text = screen.getAllByText(/Sign Up/i);
+    expect(text).toBeTruthy();
+  });
+
+  it('Renders the Checkout page', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <CheckoutPage />
+        </BrowserRouter>
+      </Provider>
+    );
+    const text = screen.getByText(/Product/i);
+    expect(text).toBeInTheDocument();
+  })
+})
