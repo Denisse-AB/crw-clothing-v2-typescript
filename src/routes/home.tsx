0db082cf70a57b258915318b9db24d5828041935
Denisse Abreu <denisseabreu21@yahoo.com>
2023-05-23 08:14:49 -0400

    Delete: todo's

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -2,8 +2,7 @@ import { Outlet } from 'react-router-dom';
 import Directory from '../components/directory/directory';
 
 import '../App.css';
-// TODO: INIT THE PROJECT AGAIN IN FIREBASE (firebase init) AND DEPLOY TO AND DEPLOY TO A PRIVIEW CHANNEL.
-// Remember to change node version!!!
+
 const Home = () => {
   return (
     <div>
